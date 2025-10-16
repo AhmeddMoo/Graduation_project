16e371bc68718e3f9c5d74d6254130ada5c76bc4
AhmeddMoo <ahmeddmohamedd695@gmail.com>
2025-10-16 22:05:22 +0300

    Fixed CSS static file path in HTML templates for production.

--- Graduation_Project/settings.py
+++ Graduation_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-+9l)n#gtn1r985h+_jrssbfqa#899!cfjh)(fquk5_6iht7)z-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ahmedmo.pythonanywhere.com"]
+ALLOWED_HOSTS = ["ahmedmo.pythonanywhere.com","127.0.0.1", "localhost"]
 
 
 # Application definition
